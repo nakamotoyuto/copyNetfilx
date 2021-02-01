075346052a372930fc2a80a5269ecdbb3fef4ff5
仲本勇斗 <tenisu0303@icloud.com>
2021-02-02 00:22:34 +0900

    complete step4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Row } from './Row';
+import { requests } from './request'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
--- src/request.ts
+++ src/request.ts
@@ -1,12 +1,12 @@
-const API_KEY = "";
+const API_KEY = "27064cae67c64faae3a429608cf2d9a0";
 
 export const requests = {
-  feachTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  feactTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-  feactActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  feactComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  feactHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-  feactRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-  feactDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+  feachTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  feachNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  feactTopRated: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&languager=en-us`,
+  feactActionMovies: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=28`,
+  feactComedyMovies: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=35`,
+  feactHorrorMovies: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=27`,
+  feactRomanceMovies: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+  feactDocumentMovies: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=99`,
 }
